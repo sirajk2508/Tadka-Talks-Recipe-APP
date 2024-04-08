7e5b76d00cf5bb3650485e91472dac095e6806a3
sirajk2508 <sirajkhan3649@gmail.com>
2024-04-09 00:07:17 +0530

    Responsiveness added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,24 +3,31 @@ import { BACKGROUND_IMG_BANNER } from "../libs/constants.js";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="relative">
       <img
         alt="background-banner"
         src={BACKGROUND_IMG_BANNER}
-        className="w-full h-[100vh] absolute"
+        className="w-full h-[100vh] object-cover"
       />
-      <div className="relative px-96 py-52">
-        <h1 className="font-primary text-8xl font-extrabold">
-          Cooking Made Simple
-        </h1>
-        <p className="text-2xl text-center py-5 text-gray-700 font-semibold">
-          Delicious, simple recipes that will make you look like a kitchen
-          goddess!
-        </p>
-        <Link to="/listedrecipe"><button className='bg-red-500 text-white px-6 py-2 rounded-lg ml-[19rem] hover:font-semibold'>Start now</button></Link>
+      <div className="absolute inset-0 flex flex-col justify-center items-center">
+        <div className="text-center">
+          <h1 className="font-primary text-5xl md:text-8xl font-extrabold">
+            Cooking Made Simple
+          </h1>
+          <p className="text-lg md:text-2xl text-gray-700 font-semibold py-5 px-4 md:px-0">
+            Delicious, simple recipes that will make you look like a kitchen
+            goddess!
+          </p>
+          <Link to="/listedrecipe">
+            <button className="bg-red-500 text-white px-6 py-2 rounded-lg hover:font-semibold">
+              Start now
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
 export default HomePage;
+
