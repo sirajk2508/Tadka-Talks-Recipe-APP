80ed5e44bc4f5256a2cac13c891ccd2a1fa76e2f
sirajk2508 <sirajkhan3649@gmail.com>
2024-04-09 00:10:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -36,9 +36,3 @@ function App() {
 }
 
 export default App;
-
-/*
-https://api.spoonacular.com/recipes/random?apiKey=1d1f9d12eb34430f8e520ae79d1145f2&number=1&include-tags=vegetarian,dessert&exclude-tags=quinoa
-
-https://api.spoonacular.com/recipes/complexSearch?apiKey=1d1f9d12eb34430f8e520ae79d1145f2&query=pasta&maxFat=25&number=2
-*/
